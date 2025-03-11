8f1720c120d8b4c34ae181fa4babf473675270db
ehg54 <ehg54@cornell.edu>
2025-03-11 18:32:03 -0400

    inliner

--- skeleton/Skeleton.cpp
+++ skeleton/Skeleton.cpp
@@ -3,21 +3,50 @@
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/Transforms/Utils/Cloning.h"
+#include "llvm/Analysis/InlineCost.h"
+#include "llvm/IR/InstIterator.h"
 
 using namespace llvm;
 
 namespace {
 
 struct SkeletonPass : public PassInfoMixin<SkeletonPass> {
+    // static char ID;
+    // SkeletonPass() : FunctionPass(ID) {}
+
     PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM) {
-        for (auto &F : M) {
-            errs() << "I saw a function called " << F.getName() << "!\n";
+        for (auto& F : M) {
+            errs() << "saw a function called " << F.getName() << "!\n";
+            std::vector<Instruction *> worklist;
+            for (inst_iterator I = inst_begin(F), E = inst_end(F); I != E; ++I) {
+                worklist.push_back(&*I);
+            }
+            for (auto I : worklist) {
+                if (auto* caller = dyn_cast<CallInst>(I)) {
+                    Function* callee = caller->getCalledFunction();
+                    if (callee->getInstructionCount() < 8 && isInlineViable(*callee).isSuccess()) {
+                        // inline the function if it is short
+                        InlineFunctionInfo ifi;
+                        InlineFunction(*caller, ifi);
+                    }
+                }
+            }
+            
         }
-        return PreservedAnalyses::all();
+        
+        return PreservedAnalyses::none();
     };
 };
 
 }
+
+// char SkeletonPass::ID = 0;
+// static void registerSkeletonPass(const PassManagerBuilder &, legacy::PassManagerBase &PM) {
+//     PM.add(new SkeletonPass());
+// }
+
+// static RegisterStandardPasses RegisterMyPass(PassManagerBuilder::EP_EarlyAsPossible, registerSkeletonPass);
 
 extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
 llvmGetPassPluginInfo() {
